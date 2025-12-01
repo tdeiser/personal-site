bcebc970e46e5827bec250e8a61dac9f54b7b906
tdeiser <thomas.p.deiser@gmail.com>
2025-12-01 18:08:24 -0500

    adjustment for tailwind compatability

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  // Tell Next where the app lives in production (GitHub Pages subfolder)
+  basePath: isProd ? "/personal-site" : undefined,
+  assetPrefix: isProd ? "/personal-site/" : undefined,
 };
 
 export default nextConfig;
